bdb881e584dc878fe09cace13f3f62b9287e9ad4
Oleg Beloglazov <r6710997@gmail.com>
2026-02-01 17:38:58 +0700

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,15 @@
+/**
+ * @author Oleg Beloglazov
+ * @version 1.0.2026
+ */
+// Это класс Main
+public class Program {
+    // Метод main - это точка входа в программу
+    public static void main(String[] args) {
+        /*
+        Инструкция system.out.println выводит текст в консоль
+        и в конце добавляет символ перевод перевода строки
+         */
+        System.out.println("My name is Oleg Beloglazov");
+    }
+}
